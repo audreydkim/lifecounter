13d08a2fe4b6a64bf271863955068976ecd48b2b
audreydk <audreydk@cs.washington.edu>
2024-04-17 13:24:01 -0700

    establish buttons and connect some labels + buttons to viewcontroller

--- lifecounter/lifecounter/ViewController.swift
+++ lifecounter/lifecounter/ViewController.swift
@@ -14,6 +14,27 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    // All for Player 1 Section
+    @IBOutlet weak var player1_label: UILabel!
+    
+    @IBOutlet weak var player1_lflabel: UILabel!
+    
+    // put counter connector here
+    
+    
+    
+    // All for Player 2 Section
+    @IBOutlet weak var player2_label: UILabel!
+    
+    @IBOutlet weak var player2_lflabel: UILabel!
+    
+    // put counter connector here
+    
+    
+    
+    
+    
+    
+    
 }
 
