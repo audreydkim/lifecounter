28c65a63adc5f5bea9d4857cca4ce9f74d9875d1
audreydk <audreydk@cs.washington.edu>
2024-04-17 14:42:53 -0700

    fully working game

--- lifecounter/lifecounter/ViewController.swift
+++ lifecounter/lifecounter/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     // fields keeping track of player totals
     var p1_curr : Int = 20
     var p2_curr : Int = 20
-    var loser : String = "your mom"
+    var loser : String = "ß"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,21 +21,87 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var gameOver: UILabel!
     
-    // All for Player 1 Section
+    // All for Player 1 Section--------------------
     @IBOutlet weak var player1_label: UILabel!
-    
     @IBOutlet weak var player1_lflabel: UILabel!
     
     // total:
     @IBOutlet weak var player1_total: UILabel!
     
-    // edits p1_curr
-    // then changes player1_lflabel
+    // SUBTRACTING BUTTONS
     @IBAction func subtract5_p1(_ sender: Any) {
-        p1_curr -= 5
+        if (p1_curr - 5 < 0) {
+            p1_curr = 0
+        } else {
+            p1_curr -= 5
+        }
         player1_total.text = String(p1_curr)
         gameOverChecker()
     }
+    @IBAction func subract1_p1(_ sender: Any) {
+        if (p1_curr - 1 < 0) {
+            p1_curr = 0
+        } else {
+            p1_curr -= 1
+        }
+        player1_total.text = String(p1_curr)
+        gameOverChecker()
+    }
+    
+    // ADDITION BUTTONS
+    @IBAction func add5_p1(_ sender: Any) {
+        gameRestarting(player: 1)
+        p1_curr += 5
+        player1_total.text = String(p1_curr)
+    }
+    @IBAction func add1_p1(_ sender: Any) {
+        gameRestarting(player: 1)
+        p1_curr += 1
+        player1_total.text = String(p1_curr)
+    }
+    
+    
+    
+    // All for Player 2 Section---------------------
+    @IBOutlet weak var player2_label: UILabel!
+    @IBOutlet weak var player2_lflabel: UILabel!
+    
+    // total:
+    @IBOutlet weak var player2_total: UILabel!
+    
+    // SUBTRACTING BUTTONS
+    @IBAction func subtract5_p2(_ sender: Any) {
+        if (p2_curr - 5 < 0) {
+            p2_curr = 0
+        } else {
+            p2_curr -= 5
+        }
+        player2_total.text = String(p2_curr)
+        gameOverChecker()
+    }
+    @IBAction func subtract1_p2(_ sender: Any) {
+        if (p2_curr - 1 < 0) {
+            p2_curr = 0
+        } else {
+            p2_curr -= 1
+        }
+        player2_total.text = String(p2_curr)
+        gameOverChecker()
+    }
+    
+    // ADDITION BUTTONS
+    @IBAction func add5_p2(_ sender: Any) {
+        gameRestarting(player: 2)
+        p2_curr += 5
+        player2_total.text = String(p2_curr)
+    }
+    
+    @IBAction func add1_p2(_ sender: Any) {
+        gameRestarting(player: 2)
+        p2_curr += 1
+        player2_total.text = String(p2_curr)
+    }
+    
     
     func gameOverChecker() {
         if (p1_curr <= 0) {
@@ -45,25 +111,15 @@ class ViewController: UIViewController {
             loser = "2"
             gameOver.text = "Player \(loser) LOSES!"
         }
-        print(p1_curr <= 0)
     }
     
-    
-    
-    
-    
-    
-    // All for Player 2 Section
-    @IBOutlet weak var player2_label: UILabel!
-    
-    @IBOutlet weak var player2_lflabel: UILabel!
-    
-    // total:
-    @IBOutlet weak var player2_total: UILabel!
-    
-    
-    
-    
+    func gameRestarting(player: Int) {
+        if (p1_curr <= 0 && player == 1) {
+            gameOver.text = ""
+        } else if (p2_curr <= 0 && player == 2) {
+            gameOver.text = ""
+        }
+    }
     
     
 }
